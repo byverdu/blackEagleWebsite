c7dfaec5817384f72dcac443ead06e1e3b91fde3
byverdu <byverdu@gmail.com>
2017-01-29 00:46:09 +0000

    setup development env, needs cleanup for gulp tasks

---
+++ gulp/tasks/paths.js
@@ -0,0 +1,16 @@
+// Paths used for the gulp tasks
+const srcDir = './src';
+const destDir = './dist';
+
+module.exports = {
+  srcJs: `${srcDir}/prettyError.js`,
+  destJs: destDir,
+  srcSass: `${srcDir}/styles.scss`,
+  destSass: destDir,
+  srcCss: `${destDir}/styles.css`,
+  destCss: destDir,
+  cleanDir: destDir,
+  srcHtml: 'index.html',
+  destHtml: destDir,
+  srcServe: destDir,
+};
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,15 @@
+const gulp = require( 'gulp' );
+const paths = require( '../paths' );
+const reload = require( 'browser-sync' ).reload;
+
+// outputs changes to files to the console
+function reportChange( event ) {
+  console.log( `File ${event.path} was ${event.type} \nrunning tasks...` );
+}
+
+gulp.task( 'watch', () => {
+  gulp.watch( paths.srcJs, [ 'minify', 'move-js', reload ]).on( 'change', reportChange );
+  gulp.watch( paths.srcSass, [ 'sass', 'css-minify', reload ]).on( 'change', reportChange );
+  gulp.watch( paths.srcHtml, [ 'move-html', reload ]).on( 'change', reportChange );
+  gulp.watch( paths.watchMocha, reload ).on( 'change', reportChange );
+});
